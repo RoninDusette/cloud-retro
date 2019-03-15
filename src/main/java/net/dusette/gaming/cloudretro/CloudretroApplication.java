10bf3f016f4a1229ee2730a2641b50ce01c49bb6
Ronin Dusette <ronindusette@Ronins-MacBook-Pro.local>
2019-03-14 21:20:54 -0700

    initial commit

---
+++ src/main/java/net/dusette/gaming/cloudretro/CloudretroApplication.java
@@ -0,0 +1,13 @@
+package net.dusette.gaming.cloudretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudretroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudretroApplication.class, args);
+    }
+
+}
